e4d47243f29b13bd2c25d8324df41ba48c09bab1
Steffen Nielsen <1061887@ucn.dk>
2018-06-12 14:17:48 +0200

    MongoDB added
    MongoDB added

---
+++ Optimal Gaming WCF/Optimal Gaming Class Library/Database Layer/MongoDB.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optimal_Gaming_Class_Library.Database_Layer
+{
+    public class MongoDB
+    {
+        public string ConnectionString = "mongodb+srv://stef4905:xep24mkn@cluster0-bjeod.mongodb.net/test?retryWrites=true";
+
+        public MongoClient GetConnection()
+        {
+            return new MongoClient("mongodb+srv://stef4905:xep24mkn@cluster0-bjeod.mongodb.net/test?retryWrites=true");
+        }
+
+    }
+}
