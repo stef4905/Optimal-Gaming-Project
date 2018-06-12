e894b250e5bf2021574d7b52df9b3875bc5a08bd
Steffen Nielsen <steffennielsen95@gmail.com>
2018-06-12 21:18:58 +0200

    small update
    ss

---
+++ Optimal Gaming WCF/Console App for testing only/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_App_for_testing_only
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
