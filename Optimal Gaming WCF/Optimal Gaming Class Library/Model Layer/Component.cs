2644248468eed191bbbf2a302adbd35fb947d4c9
Steffen Nielsen <steffennielsen95@gmail.com>
2018-06-18 11:01:10 +0200

    Components class T is created
    This class will be the one to contain the specific component, with it's name etc.

---
+++ Optimal Gaming WCF/Optimal Gaming Class Library/Model Layer/Component.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optimal_Gaming_Class_Library.Model_Layer
+{
+    public class Component<T>
+    {
+        [BsonId]
+        public ObjectId MongoDBId { get; set; } // MongoDB Id generated automaticly on insert into the databse. 
+        public T ComponentType { get; set; }
+        public string Name { get; set; }
+        public string ImageLink { get; set;}
+        public string AfffiliateLink { get; set; }
+    }
+}
