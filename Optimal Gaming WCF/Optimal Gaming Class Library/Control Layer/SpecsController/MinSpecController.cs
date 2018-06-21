ff88bd9cfab666bb861e14b09628ea33fed86f0f
MVN97 <31692504+MVN97@users.noreply.github.com>
2018-06-21 13:24:02 +0200

    dims
    sdsda

---
+++ Optimal Gaming WCF/Optimal Gaming Class Library/Control Layer/SpecsController/MinSpecController.cs
@@ -0,0 +1,25 @@
+﻿using Optimal_Gaming_Class_Library.Database_Layer;
+using Optimal_Gaming_Class_Library.Model_Layer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optimal_Gaming_Class_Library.Control_Layer.SpecsController
+{
+    public class MinSpecController
+    {
+        private MongoDBMinSpec MinSpecs;
+
+        public MinSpecController()
+        {
+            MinSpecs = new MongoDBMinSpec();
+        }
+
+        public void CreateMinSpec(Spec obj)
+        {
+            MinSpecs.Create(obj);
+        }
+    }
+}
