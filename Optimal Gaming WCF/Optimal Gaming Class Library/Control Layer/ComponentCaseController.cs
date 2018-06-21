8c22b3a12ec924a97cc0157e9f5ad561557b83d1
Steffen Nielsen <steffennielsen95@gmail.com>
2018-06-21 13:45:41 +0200

    Controller for CPU GPU and Case created
    1

--- Optimal Gaming WCF/Optimal Gaming Class Library/Control Layer/ComponentCaseController.cs
+++ Optimal Gaming WCF/Optimal Gaming Class Library/Control Layer/ComponentCaseController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using Optimal_Gaming_Class_Library.Database_Layer.Components;
+using Optimal_Gaming_Class_Library.Model_Layer.Components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,51 @@ using System.Threading.Tasks;
 
 namespace Optimal_Gaming_Class_Library.Control_Layer
 {
-    class ComponentCaseController
+    public class ComponentCaseController
     {
+        private MongoDBCase _MongoDBCase = null;
+
+        public ComponentCaseController()
+        {
+            _MongoDBCase = new MongoDBCase();
+        }
+
+        /// <summary>
+        /// Add the igven Case object to the database
+        /// </summary>
+        /// <param name="obj">Case object</param>
+        public void AddToDatabase(Case obj)
+        {
+            _MongoDBCase.Create(obj);
+        }
+
+        /// <summary>
+        /// Return a single Case object from the database by the given ObjectId
+        /// </summary>
+        /// <param name="id">ObjectId of the document</param>
+        /// <returns>Case object</returns>
+        public Case GetCaseFromDatabase(ObjectId id)
+        {
+            return _MongoDBCase.Get(id);
+        }
+
+        /// <summary>
+        /// Updates the given Case object in the database
+        /// </summary>
+        /// <param name="obj">Case object to update</param>
+        public async void UpdateCase(Case obj)
+        {
+            await _MongoDBCase.UpdateAsync(obj);
+        }
+
+        /// <summary>
+        /// Deletes the given Case from the database if it exists
+        /// </summary>
+        /// <param name="obj">Case object to delete</param>
+        /// <returns>Bool true = success, false = failed</returns>
+        public bool DeleteFromDatabase(Case obj)
+        {
+            return _MongoDBCase.Delete(obj);
+        }
     }
 }
