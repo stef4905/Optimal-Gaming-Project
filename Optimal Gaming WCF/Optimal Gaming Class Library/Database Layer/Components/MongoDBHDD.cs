5e060773702fd527ea0ffb9a362b3c0366c8a149
Steffen Nielsen <steffennielsen95@gmail.com>
2018-06-21 10:54:07 +0200

    MongoDB components
    Classes created but not yet implemented their needed metohds.

---
+++ Optimal Gaming WCF/Optimal Gaming Class Library/Database Layer/Components/MongoDBHDD.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optimal_Gaming_Class_Library.Database_Layer.Components
+{
+    class MongoDBHDD
+    {
+    }
+}
