efb4d0f9eb8a3044238a290fded93d7faef40b04
Steffen Nielsen <steffennielsen95@gmail.com>
2018-06-21 12:53:49 +0200

    WCF deleted and new console app created
    22

---
+++ Optimal Gaming WCF/Console App for testing i only - Mathias/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_App_for_testing_i_only___Mathias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
