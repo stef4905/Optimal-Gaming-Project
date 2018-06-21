6bc6ddfe529d2830e5f8d6dee31f8397ef32d0c8
Steffen Nielsen <steffennielsen95@gmail.com>
2018-06-21 13:23:58 +0200

    1
    1

---
+++ Optimal Gaming WCF/Optimal Gaming Class Library/Control Layer/ComponentCaseController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optimal_Gaming_Class_Library.Control_Layer
+{
+    class ComponentCaseController
+    {
+    }
+}
