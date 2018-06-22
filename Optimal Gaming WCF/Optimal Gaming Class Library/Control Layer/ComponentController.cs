b6ebf054dfa68b55236989dd952252b2612fba78
Steffen Nielsen <steffennielsen95@gmail.com>
2018-06-22 12:21:48 +0200

    ComponentController created
    ComponentController created and will handle the combination functionalities for the different component controllers.

---
+++ Optimal Gaming WCF/Optimal Gaming Class Library/Control Layer/ComponentController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optimal_Gaming_Class_Library.Control_Layer
+{
+    public class ComponentController
+    {
+        private ComponentCaseController _ComponentCaseController = null;
+        private ComponentGPUController _ComponentGPUController = null;
+        private ComponentCPUController _ComponentCPUController = null;
+        private ComponentMotherboardController _ComponentMotherboardController = null;
+        private ComponentPSUController _ComponentPSUController = null;
+        private ComponentSizeController _ComponentSizeController = null;
+        private ComponentSSDController _ComponentSSDController = null;
+        private ComponentHDDController _ComponentHDDController = null;
+        
+        /// <summary>
+        /// Constructor of the ComponentController class. Instanciate the different component controllers.
+        /// </summary>
+        public ComponentController()
+        {
+            _ComponentCaseController = new ComponentCaseController();
+            _ComponentGPUController = new ComponentGPUController();
+            _ComponentCPUController = new ComponentCPUController();
+            _ComponentMotherboardController = new ComponentMotherboardController();
+            _ComponentPSUController = new ComponentPSUController();
+            _ComponentSizeController = new ComponentSizeController();
+            _ComponentSSDController = new ComponentSSDController();
+            _ComponentHDDController = new ComponentHDDController();
+        }
+    }
+}
---
+++ Optimal Gaming WCF/Optimal Gaming Class Library/Control Layer/ComponentHDDController.cs
@@ -0,0 +1,64 @@
+﻿using MongoDB.Bson;
+using Optimal_Gaming_Class_Library.Database_Layer.Components;
+using Optimal_Gaming_Class_Library.Model_Layer.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optimal_Gaming_Class_Library.Control_Layer
+{
+    public class ComponentHDDController
+    {
+        private MongoDBHDD _MongoDBHDD = null;
+
+        /// <summary>
+        /// Constructor for the ComponentHDDController
+        /// </summary>
+        public ComponentHDDController()
+        {
+            _MongoDBHDD = new MongoDBHDD();
+        }
+
+        /// <summary>
+        /// Adds the given HDD object to the database
+        /// </summary>
+        /// <param name="obj">HDD object to insert into the database</param>
+        public void AddToDatabase(HDD obj)
+        {
+            _MongoDBHDD.Create(obj);
+        }
+
+        /// <summary>
+        /// Returns a single HDD object from the database by the given ObjectId
+        /// </summary>
+        /// <param name="id">ObjectId of the document in the database</param>
+        /// <returns>HDD object</returns>
+        public HDD GetHDD(ObjectId id)
+        {
+            return _MongoDBHDD.Get(id);
+        }
+
+        /// <summary>
+        /// Updates the given HDD object in the database if it exists
+        /// </summary>
+        /// <param name="obj">HDD object to update</param>
+        public async void Update(HDD obj)
+        {
+            await _MongoDBHDD.UpdateAsync(obj);
+        }
+        
+        /// <summary>
+        /// Deletes the given HDD object from the database if it exists
+        /// </summary>
+        /// <param name="obj">HDD object to delete</param>
+        /// <returns>Bool - true = success, false = failed</returns>
+        public bool DeleteFromDatabase(HDD obj)
+        {
+            return _MongoDBHDD.Delete(obj);
+        }
+
+
+    }
+}
