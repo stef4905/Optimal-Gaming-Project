65f46fab6df2e211a742ebc963d283912ea54aca
Steffen Nielsen <steffennielsen95@gmail.com>
2018-07-07 00:11:34 +0200

    Creation of a new Case component now works.
    Creation of a new Case component now works perfectly. Now we just need the rest to work. But by following what i have done in this scenario gives a good understanding about how the rest should be made accordingly.

--- Optimal Gaming WCF/Content Management System/PcParts.xaml.cs
+++ Optimal Gaming WCF/Content Management System/PcParts.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Optimal_Gaming_Class_Library.Control_Layer;
+using Optimal_Gaming_Class_Library.Model_Layer.Components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,11 +23,31 @@ namespace Content_Management_System
     public partial class PcParts : UserControl
     {
         Grid MainGrid = null;
+        ComponentController _ComponentController = new ComponentController();
         
         public PcParts(Grid mainGrid)
         {
             InitializeComponent();
             MainGrid = mainGrid;
+            GetAllSpecsAndRefresh();
+        }
+
+        /// <summary>
+        /// Displayes all pc parts from the mongo db in the table list
+        /// </summary>
+        private void GetAllSpecsAndRefresh()
+        {
+            //Clear the table first
+            TableList.Items.Clear();
+
+            //Get all pc parts through their individual controller
+            List<Case> Case = _ComponentController._ComponentCaseController.GetAllCase();
+
+            //Foreach over each pc part list and insert them into the table
+            foreach (var _case in Case)
+            {
+                TableList.Items.Add(_case);
+            }
         }
 
         private void SearchForPart(object sender, RoutedEventArgs e)
@@ -40,7 +62,17 @@ namespace Content_Management_System
 
         private void DeleteSelectedPart(object sender, RoutedEventArgs e)
         {
+            //Get the selected item in the list
+            var selectedItem = TableList.SelectedItem;
 
+            //Check for the type of the object. Used to dertermain wich controller to act on.
+            if (selectedItem.GetType() == typeof(Case))
+            {
+                _ComponentController._ComponentCaseController.DeleteFromDatabase(selectedItem as Case);
+            }
+
+            //Refresh the list of all the pc parts
+            GetAllSpecsAndRefresh();
         }
 
         private void EditSelectedPart(object sender, RoutedEventArgs e)
--- Optimal Gaming WCF/Content Management System/UserControls/CaseComponent.xaml.cs
+++ Optimal Gaming WCF/Content Management System/UserControls/CaseComponent.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Optimal_Gaming_Class_Library.Control_Layer;
+using Optimal_Gaming_Class_Library.Model_Layer.Components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,6 +22,9 @@ namespace Content_Management_System.UserControls
     /// </summary>
     public partial class CaseComponent : UserControl
     {
+
+        private ComponentController _ComponentController = new ComponentController();
+
         public CaseComponent()
         {
             InitializeComponent();
@@ -27,6 +32,25 @@ namespace Content_Management_System.UserControls
 
         private void AddSpecButton(object sender, RoutedEventArgs e)
         {
+            Console.WriteLine("Doing the stuff");
+            // Create size object from the text inputs
+            Optimal_Gaming_Class_Library.Model_Layer.Components.Details.Size size = new Optimal_Gaming_Class_Library.Model_Layer.Components.Details.Size(Convert.ToDouble(HeightText.Text), Convert.ToDouble(WidthText.Text), Convert.ToDouble(DepthText.Text), Convert.ToDouble(WeightText.Text));
+            NameText.Text = "2";
+
+            //Split the text field that will end up as string arrays
+            string[] compatibleMotherboards = CompatibleMotherboardsText.Text.Split(',');
+            string[] material = MaterialText.Text.Split(',');
+            string[] cooling = CoolingText.Text.Split(',');
+            string[] storageExpansion = StorageExpantionText.Text.Split(',');
+            string[] _interface = InterfaceText.Text.Split(',');
+
+            // Create new Case object from the text inputs
+            Case _case = new Case(NameText.Text, Convert.ToDouble(PriceText.Text), BrandNameText.Text, size, ModelText.Text, compatibleMotherboards, material, cooling, Convert.ToInt32(MaxCPUCoolerHeightText.Text), Convert.ToInt32(MaxGPULengthText.Text), storageExpansion, _interface);
+
+            //Create the new case in mongo database
+            _ComponentController._ComponentCaseController.AddToDatabase(_case);
+            Console.WriteLine("Case has been added to the database");
+
 
         }
     }
